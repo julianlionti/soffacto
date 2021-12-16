c45e7b0b12756ad6a8070ca6b6ae03eb2816f35d
Julian Lionti <julliont@publicisgroupe.net>
2021-12-16 00:06:31 -0300

    welcome msg

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,11 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { initTranslations } from "./translations";
 
-console.log("----------------Welcome to Soffacto!----------------");
+console.log(
+  "%c" + "Welcome to Soffacto",
+  "font-family:Comic Sans MS; font-size:50px; font-weight:bold; color:blue"
+);
+
 initTranslations();
 const app = document.getElementById("app");
 ReactDOM.render(<App />, app);
